6dafeae912164b517ff7611cfe243f6946a7ff7a
Ian Murray <ianmurrays@gmail.com>
2011-02-21 11:49:18 -0300

    Changing default route to login form

--- config/routes.rb
+++ config/routes.rb
@@ -66,7 +66,7 @@ Invoices::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => "welcome#index"
+  root :to => "sessions#new"
 
   # See how all your routes lay out with "rake routes"
 
